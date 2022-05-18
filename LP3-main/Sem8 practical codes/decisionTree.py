40600b4b9bf9507d867a86de72b2459ac8a3c89d
sameer9890 <65761055+sameer9890@users.noreply.github.com>
2022-05-18 15:26:01 +0530

    Add files via upload

---
+++ LP3-main/Sem8 practical codes/decisionTree.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("DataTable.csv")
+x = data.iloc[:,:-1]
+y = data.iloc[:,5]
+x = x.apply(LabelEncoder().fit_transform)
+print(x)
+x_in = [1,1,0,0]
+model = DecisionTreeClassifier()
+model.fit(x.iloc[:,1:5].values,y.values)
+print() #Empty Line
+print("\nAge < 21, Income = Low, Gender = Female, Marital Status = Married, Buys:",model.predict([x_in]))
+
+print() #Empty Line
+plot_tree(model)
+plt.show()
